b24886a0e2e8e3fef77bd31b2667b0009f1d2efb
Ryan Ford <59795228+ryanmford@users.noreply.github.com>
2026-07-03 09:12:49 -1000

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/apex-movement/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
